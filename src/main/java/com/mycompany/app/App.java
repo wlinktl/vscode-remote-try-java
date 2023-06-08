d6242d9fecf1d6f98e57e141c09132a3093a7465
wlinktl <wlinkcl@outlook.com>
2023-06-08 16:15:52 +0000

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,5 +8,25 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
+      
+    }
+
+    public void loop(){
+         //create loop from 1 to 100 of integer
+        for (int i = 1; i <= 100; i++) {
+            //if the number is divisible by 3 and 5, print FizzBuzz
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            //if the number is divisible by 3, print Fizz
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            //if the number is divisible by 5, print Buzz
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            //if the number is not divisible by 3 or 5, print the number
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
