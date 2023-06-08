623819727a6e5e9e6c6aac9a0e654653c9ed7ac2
wlinktl <wlinkcl@outlook.com>
2023-06-08 14:57:29 -0400

    102

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -33,7 +33,25 @@ public class App {
 
     public void list()
     {
-        //create list of integer from 1 to 100 101
+        //create list of integer from 1 to 100 
+        for (int i = 1; i <= 100; i++) {
+            //if the number is divisible by 3 and 5, print FizzBuzz
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            //if the number is divisible by 3, print Fizz
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            //if the number is divisible by 5, print Buzz
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            //if the number is not divisible by 3 or 5, print the number
+            } else {
+                System.out.println(i);
+            }
+        }
+ 
+         
+
         
         
         
